135ae003cab221aefd7817e3424542973b8d6e95
anastasiiaipe <143907938+anastasiiaipe@users.noreply.github.com>
2024-04-26 18:36:08 +0200

    up delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,8 @@
-// import { useEffect } from "react";
-// import ContactList from "./components/ContactList/ContactList";
-
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 
 import "./App.css";
-// import SearchBox from "./components/SearchBox/SearchBox";
-// import ContactForm from "./components/ContactForm/ContactForm";
-
-// function App() {
-//   // const [contacts, setContacts] = useState(() => {
-//   //   const storedContacts = localStorage.getItem("contacts");
-//   //   return storedContacts ? JSON.parse(storedContacts) : contactsData;
-//   // });
-//   const [filter, setFilter] = useState("");
-
-//   useEffect(() => {
-//     localStorage.setItem("contacts", JSON.stringify(contacts));
-//   }, [contacts]);
-
-//   const addContact = (newContact) => {
-//     setContacts([...contacts, newContact]);
-//   };
-
-//   const handleDeleteContact = (id) => {
-//     setContacts(contacts.filter((contact) => contact.id !== id));
-//   };
-
-//   const handleFilterChange = (value) => {
-//     setFilter(value);
-//   };
-
-//   const filteredContacts = contacts.filter((contact) =>
-//     contact.name.toLowerCase().includes(filter.toLowerCase())
-//   );
-
-//   return (
-//     <div>
-//       <h1>Phonebook</h1>
-//       <ContactForm onSubmit={addContact} />
-//       <SearchBox value={filter} onChange={handleFilterChange} />
-//       <ContactList contacts={filteredContacts} onDelete={handleDeleteContact} />
-//     </div>
-//   );
-// }
-
-// export default App;
 
 function App() {
   return (
--- src/components/redux/contactsSlice.js
+++ src/components/redux/contactsSlice.js
@@ -16,11 +16,11 @@ const contactSlice = createSlice({
     addContact(state, action) {
       state.items.push(action.payload);
     },
-  },
-  deleteContact(state, action) {
-    state.items = state.items.filter(
-      (contact) => contact.id !== action.payload
-    );
+    deleteContact(state, action) {
+      state.items = state.items.filter(
+        (contact) => contact.id !== action.payload
+      );
+    },
   },
 });
 
